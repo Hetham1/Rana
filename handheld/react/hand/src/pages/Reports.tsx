7d28168be29275435a5dbebe4ee24673afe47256
Hethampc <jopjopjop372@gmail.com>
2024-10-06 11:13:41 -0700

    working demo of report

--- handheld/react/hand/src/pages/Reports.tsx
+++ handheld/react/hand/src/pages/Reports.tsx
@@ -22,11 +22,9 @@ import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
 
 import { BASE_URL } from '../hooks/apiconfig';
 
-// Define the structure of your table data
+// Define a flexible structure for table data
 interface TableDataItem {
-  column1: string;
-  column2: string;
-  column3: string;
+  [key: string]: any;
 }
 
 export default function Gozaresh() {
@@ -38,7 +36,7 @@ export default function Gozaresh() {
   const [parameter4, setParameter4] = useState('');
   const [parameter5, setParameter5] = useState('');
   const [parameter6, setParameter6] = useState('');
-  const [tableData, setTableData] = useState<TableDataItem[]>([]);
+  const [tableData, setTableData] = useState<TableDataItem[][]>([]); // Nested array to hold all the types of data
 
   const handleSubmit = async () => {
     try {
@@ -50,17 +48,14 @@ export default function Gozaresh() {
         },
       });
   
-      const data: TableDataItem[] = response.data;
+      const data: TableDataItem[][] = response.data.data;
       setTableData(data);
       setSidebarOpen(false);
     } catch (error: any) {
-      // Check if the error has a response
       if (error.response && error.response.data && error.response.data.error) {
         const errorMessage = error.response.data.error;
-        // Display the error message (you can use alert or set a state)
         alert(errorMessage);
       } else {
-        // Fallback for unexpected errors
         alert('An unexpected error occurred');
       }
     }
@@ -80,53 +75,52 @@ export default function Gozaresh() {
           <h2 className="text-lg text-left font-semibold mb-4">Parameters</h2>
           <div className="space-y-4">
             <Input
-            className='text-center'
+              className='text-center'
               placeholder="wpid"
               value={parameter1}
               onChange={(e) => setParameter1(e.target.value)}
             />
             <Input
               className='text-center'
-              
               value={parameter2}
               onChange={(e) => setParameter2(e.target.value)}
               type='date'
             />
             <div>
-            <div className="w-full flex justify-center">
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button variant="outline" className="w-full">
-                    {parameter3 || 'سکتور را انتخاب کنید'}
-                    <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] p-0">
-                  <Command>
-                    <CommandInput placeholder="جستجو" className="h-9" />
-                    <CommandList>
-                      <CommandEmpty>No letter found.</CommandEmpty>
-                      <CommandGroup>
-                        {letters.map((letter) => (
-                          <CommandItem
-                            key={letter}
-                            value={letter}
-                            onSelect={(currentValue) => {
-                              setParameter3(currentValue);
-                            }}
-                          >
-                            {letter}
-                            <CheckIcon
-                              className={`ml-auto h-4 w-4 ${parameter3 === letter ? 'opacity-100' : 'opacity-0'}`}
-                            />
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-            </div>
+              <div className="w-full flex justify-center">
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button variant="outline" className="w-full">
+                      {parameter3 || 'سکتور را انتخاب کنید'}
+                      <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-[200px] p-0">
+                    <Command>
+                      <CommandInput placeholder="جستجو" className="h-9" />
+                      <CommandList>
+                        <CommandEmpty>No letter found.</CommandEmpty>
+                        <CommandGroup>
+                          {letters.map((letter) => (
+                            <CommandItem
+                              key={letter}
+                              value={letter}
+                              onSelect={(currentValue) => {
+                                setParameter3(currentValue);
+                              }}
+                            >
+                              {letter}
+                              <CheckIcon
+                                className={`ml-auto h-4 w-4 ${parameter3 === letter ? 'opacity-100' : 'opacity-0'}`}
+                              />
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </CommandList>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+              </div>
             </div>
             <Input
               className='text-center'
@@ -152,24 +146,29 @@ export default function Gozaresh() {
       </Sheet>
 
       <div className="mt-8">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Column 1</TableHead>
-              <TableHead>Column 2</TableHead>
-              <TableHead>Column 3</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {tableData.map((row, index) => (
-              <TableRow key={index}>
-                <TableCell>{row.column1}</TableCell>
-                <TableCell>{row.column2}</TableCell>
-                <TableCell>{row.column3}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        {tableData.map((dataGroup, groupIndex) => (
+          <div key={groupIndex} className="mb-8">
+            <h3 className="text-lg font-semibold mb-4">uid {groupIndex + 1}</h3>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  {Object.keys(dataGroup[0] || {}).map((column, index) => (
+                    <TableHead key={index}>{column}</TableHead>
+                  ))}
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {dataGroup.map((row, rowIndex) => (
+                  <TableRow key={rowIndex}>
+                    {Object.values(row).map((value, cellIndex) => (
+                      <TableCell key={cellIndex}>{value}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        ))}
       </div>
     </div>
   );
