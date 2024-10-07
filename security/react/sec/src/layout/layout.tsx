3f9de04a99f79aeafb60791e170fcd13574ea129
Hethampc <jopjopjop372@gmail.com>
2024-10-07 06:30:36 -0700

    sec felan dastan

--- security/react/sec/src/layout/layout.tsx
+++ security/react/sec/src/layout/layout.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import React, { useCallback } from 'react';
+import { useCallback } from 'react';
 import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
+import { LogOutIcon } from 'lucide-react';
 import type { Engine, ISourceOptions } from "tsparticles-engine";
 
 export default function Layout() {
@@ -82,16 +83,26 @@ export default function Layout() {
       />
       <header className="bg-supblue text-white p-4 flex justify-between items-center relative z-10">
         <div>
+          
+          
+          {location.pathname === '/' && (
+          <div>
           <h1 className="text-xl font-bold">خوش آمدید</h1>
-          <p>{fullName} | {date}</p>
+          </div>
+          
+        )}
+        <p>{fullName} | {date}</p>
         </div>
         {location.pathname === '/' && (
+          <div>
           <button
             onClick={handleLogout}
             className="bg-whitebox text-text py-1 text-center px-3 rounded-lg"
           >
-            خروج
+            <LogOutIcon/>
           </button>
+          </div>
+          
         )}
       </header>
 
