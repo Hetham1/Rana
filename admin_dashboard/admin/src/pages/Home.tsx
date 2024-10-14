d317d3ca8fca262a318da39127d5baacf38a4594
Hethampc <jopjopjop372@gmail.com>
2024-10-14 07:03:30 -0700

    added some basic ass charts

--- admin_dashboard/admin/src/pages/Home.tsx
+++ admin_dashboard/admin/src/pages/Home.tsx
@@ -32,239 +32,248 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
+import { Bars, Waves, Noodle, Radial } from "@/components/ui/charts"
+
 export function Home() {
   return (
-    <div className="flex flex-col min-h-screen w-full">
-      <main className="flex-1 overflow-y-auto">
+    <div className="flex flex-col w-full">
+      <main className="flex-1 overflow-y-auto bg-slate-300">
         <div className="container mx-auto p-4 md:p-8 space-y-8">
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">
-                  Total Revenue
+                  تراکنش های جاری
                 </CardTitle>
                 <DollarSign className="h-4 w-4 text-muted-foreground" />
               </CardHeader>
               <CardContent>
                 <div className="text-2xl font-bold">$45,231.89</div>
                 <p className="text-xs text-muted-foreground">
-                  +20.1% from last month
+                  +20.1% از ماه پیش
                 </p>
               </CardContent>
             </Card>
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">
-                  Subscriptions
+                  سفارشات
                 </CardTitle>
                 <Users className="h-4 w-4 text-muted-foreground" />
               </CardHeader>
               <CardContent>
                 <div className="text-2xl font-bold">+2350</div>
                 <p className="text-xs text-muted-foreground">
-                  +180.1% from last month
+                  +180.1% از ماه پیش
                 </p>
               </CardContent>
             </Card>
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                <CardTitle className="text-sm font-medium">خروج ها</CardTitle>
                 <CreditCard className="h-4 w-4 text-muted-foreground" />
               </CardHeader>
               <CardContent>
                 <div className="text-2xl font-bold">+12,234</div>
                 <p className="text-xs text-muted-foreground">
-                  +19% from last month
+                  +19% از ماه پیش
                 </p>
               </CardContent>
             </Card>
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">Active Now</CardTitle>
+                <CardTitle className="text-sm font-medium">ظرفیت انبار</CardTitle>
                 <Activity className="h-4 w-4 text-muted-foreground" />
               </CardHeader>
               <CardContent>
-                <div className="text-2xl font-bold">+573</div>
+                <div className="text-2xl font-bold">573</div>
                 <p className="text-xs text-muted-foreground">
-                  +201 since last hour
+                  از 1000
                 </p>
               </CardContent>
             </Card>
+            </div>
+  <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+    <Card className="col-span-4">
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle>تراکنش‌ها</CardTitle>
+            <CardDescription>
+              تراکنش‌های اخیر فروشگاه شما.
+            </CardDescription>
           </div>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-            <Card className="col-span-4">
-              <CardHeader>
-                <div className="flex items-center justify-between">
-                  <div>
-                    <CardTitle>Transactions</CardTitle>
-                    <CardDescription>
-                      Recent transactions from your store.
-                    </CardDescription>
-                  </div>
-                  <Button asChild size="sm">
-                    <a href="#" className="flex items-center gap-1">
-                      View All
-                      <ArrowUpRight className="h-4 w-4" />
-                    </a>
-                  </Button>
-                </div>
-              </CardHeader>
-              <CardContent>
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Customer</TableHead>
-                      <TableHead className="hidden xl:table-cell">Type</TableHead>
-                      <TableHead className="hidden xl:table-cell">Status</TableHead>
-                      <TableHead className="hidden xl:table-cell">Date</TableHead>
-                      <TableHead className="text-right">Amount</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell>
-                        <div className="font-medium">Liam Johnson</div>
-                        <div className="hidden text-sm text-muted-foreground md:inline">
-                          liam@example.com
-                        </div>
-                      </TableCell>
-                      <TableCell className="hidden xl:table-cell">Sale</TableCell>
-                      <TableCell className="hidden xl:table-cell">
-                        <Badge variant="outline">Approved</Badge>
-                      </TableCell>
-                      <TableCell className="hidden xl:table-cell">2023-06-23</TableCell>
-                      <TableCell className="text-right">$250.00</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <div className="font-medium">Olivia Smith</div>
-                        <div className="hidden text-sm text-muted-foreground md:inline">
-                          olivia@example.com
-                        </div>
-                      </TableCell>
-                      <TableCell className="hidden xl:table-cell">Refund</TableCell>
-                      <TableCell className="hidden xl:table-cell">
-                        <Badge variant="outline">Declined</Badge>
-                      </TableCell>
-                      <TableCell className="hidden xl:table-cell">2023-06-24</TableCell>
-                      <TableCell className="text-right">$150.00</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <div className="font-medium">Noah Williams</div>
-                        <div className="hidden text-sm text-muted-foreground md:inline">
-                          noah@example.com
-                        </div>
-                      </TableCell>
-                      <TableCell className="hidden xl:table-cell">Subscription</TableCell>
-                      <TableCell className="hidden xl:table-cell">
-                        <Badge variant="outline">Approved</Badge>
-                      </TableCell>
-                      <TableCell className="hidden xl:table-cell">2023-06-25</TableCell>
-                      <TableCell className="text-right">$350.00</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <div className="font-medium">Emma Brown</div>
-                        <div className="hidden text-sm text-muted-foreground md:inline">
-                          emma@example.com
-                        </div>
-                      </TableCell>
-                      <TableCell className="hidden xl:table-cell">Sale</TableCell>
-                      <TableCell className="hidden xl:table-cell">
-                        <Badge variant="outline">Approved</Badge>
-                      </TableCell>
-                      <TableCell className="hidden xl:table-cell">2023-06-26</TableCell>
-                      <TableCell className="text-right">$450.00</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <div className="font-medium">Liam Johnson</div>
-                        <div className="hidden text-sm text-muted-foreground md:inline">
-                          liam@example.com
-                        </div>
-                      </TableCell>
-                      <TableCell className="hidden xl:table-cell">Sale</TableCell>
-                      <TableCell className="hidden xl:table-cell">
-                        <Badge variant="outline">Approved</Badge>
-                      </TableCell>
-                      <TableCell className="hidden xl:table-cell">2023-06-27</TableCell>
-                      <TableCell className="text-right">$550.00</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </CardContent>
-            </Card>
-            <Card className="col-span-3">
-              <CardHeader>
-                <CardTitle>Recent Sales</CardTitle>
-              </CardHeader>
-              <CardContent className="space-y-8">
-                <div className="flex items-center">
-                  <Avatar className="h-9 w-9">
-                    <AvatarImage src="/avatars/01.png" alt="Avatar" />
-                    <AvatarFallback>OM</AvatarFallback>
-                  </Avatar>
-                  <div className="ml-4 space-y-1">
-                    <p className="text-sm font-medium leading-none">Olivia Martin</p>
-                    <p className="text-sm text-muted-foreground">
-                      olivia.martin@email.com
-                    </p>
-                  </div>
-                  <div className="ml-auto font-medium">+$1,999.00</div>
-                </div>
-                <div className="flex items-center">
-                  <Avatar className="h-9 w-9">
-                    <AvatarImage src="/avatars/02.png" alt="Avatar" />
-                    <AvatarFallback>JL</AvatarFallback>
-                  </Avatar>
-                  <div className="ml-4 space-y-1">
-                    <p className="text-sm font-medium leading-none">Jackson Lee</p>
-                    <p className="text-sm text-muted-foreground">jackson.lee@email.com</p>
-                  </div>
-                  <div className="ml-auto font-medium">+$39.00</div>
-                </div>
-                <div className="flex items-center">
-                  <Avatar className="h-9 w-9">
-                    <AvatarImage src="/avatars/03.png" alt="Avatar" />
-                    <AvatarFallback>IN</AvatarFallback>
-                  </Avatar>
-                  
-                  <div className="ml-4 space-y-1">
-                    <p className="text-sm font-medium leading-none">Isabella Nguyen</p>
-                    <p className="text-sm text-muted-foreground">
-                      isabella.nguyen@email.com
-                    </p>
-                  </div>
-                  <div className="ml-auto font-medium">+$299.00</div>
-                </div>
-                <div className="flex items-center">
-                  <Avatar className="h-9 w-9">
-                    <AvatarImage src="/avatars/04.png" alt="Avatar" />
-                    <AvatarFallback>WK</AvatarFallback>
-                  </Avatar>
-                  <div className="ml-4 space-y-1">
-                    <p className="text-sm font-medium leading-none">William Kim</p>
-                    <p className="text-sm text-muted-foreground">will@email.com</p>
-                  </div>
-                  <div className="ml-auto font-medium">+$99.00</div>
-                </div>
-                <div className="flex items-center">
-                  <Avatar className="h-9 w-9">
-                    <AvatarImage src="/avatars/05.png" alt="Avatar" />
-                    <AvatarFallback>SD</AvatarFallback>
-                  </Avatar>
-                  <div className="ml-4 space-y-1">
-                    <p className="text-sm font-medium leading-none">Sofia Davis</p>
-                    <p className="text-sm text-muted-foreground">sofia.davis@email.com</p>
-                  </div>
-                  <div className="ml-auto font-medium">+$39.00</div>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
+          <Button asChild size="sm">
+            <a href="#" className="flex items-center gap-1">
+              مشاهده همه
+              <ArrowUpRight className="h-4 w-4" />
+            </a>
+          </Button>
         </div>
-      </main>
-    </div>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>مشتری</TableHead>
+              <TableHead className="hidden xl:table-cell">نوع</TableHead>
+              <TableHead className="hidden xl:table-cell">وضعیت</TableHead>
+              <TableHead className="hidden xl:table-cell">تاریخ</TableHead>
+              <TableHead className="text-right">مبلغ</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">لیام جانسون</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  liam@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden xl:table-cell">فروش</TableCell>
+              <TableCell className="hidden xl:table-cell">
+                <Badge variant="outline">تأیید شده</Badge>
+              </TableCell>
+              <TableCell className="hidden xl:table-cell">2023-06-23</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">اولیویا اسمیت</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  olivia@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden xl:table-cell">بازپرداخت</TableCell>
+              <TableCell className="hidden xl:table-cell">
+                <Badge variant="outline">رد شده</Badge>
+              </TableCell>
+              <TableCell className="hidden xl:table-cell">2023-06-24</TableCell>
+              <TableCell className="text-right">$150.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">نوح ویلیامز</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  noah@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden xl:table-cell">اشتراک</TableCell>
+              <TableCell className="hidden xl:table-cell">
+                <Badge variant="outline">تأیید شده</Badge>
+              </TableCell>
+              <TableCell className="hidden xl:table-cell">2023-06-25</TableCell>
+              <TableCell className="text-right">$350.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">اما براون</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  emma@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden xl:table-cell">فروش</TableCell>
+              <TableCell className="hidden xl:table-cell">
+                <Badge variant="outline">تأیید شده</Badge>
+              </TableCell>
+              <TableCell className="hidden xl:table-cell">2023-06-26</TableCell>
+              <TableCell className="text-right">$450.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">لیام جانسون</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  liam@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden xl:table-cell">فروش</TableCell>
+              <TableCell className="hidden xl:table-cell">
+                <Badge variant="outline">تأیید شده</Badge>
+              </TableCell>
+              <TableCell className="hidden xl:table-cell">2023-06-27</TableCell>
+              <TableCell className="text-right">$550.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+    <Card className="col-span-3">
+      <CardHeader>
+        <CardTitle>فروش‌های اخیر</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-8">
+        <div className="flex items-center">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/avatars/01.png" alt="Avatar" />
+            <AvatarFallback>OM</AvatarFallback>
+          </Avatar>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">اولیویا مارتین</p>
+            <p className="text-sm text-muted-foreground">
+              olivia.martin@email.com
+            </p>
+          </div>
+          <div className="ml-auto font-medium">+$1,999.00</div>
+        </div>
+        <div className="flex items-center">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/avatars/02.png" alt="Avatar" />
+            <AvatarFallback>JL</AvatarFallback>
+          </Avatar>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">جکسون لی</p>
+            <p className="text-sm text-muted-foreground">jackson.lee@email.com</p>
+          </div>
+          <div className="ml-auto font-medium">+$39.00</div>
+        </div>
+        <div className="flex items-center">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/avatars/03.png" alt="Avatar" />
+            <AvatarFallback>IN</AvatarFallback>
+          </Avatar>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">ایزابلا نگوین</p>
+            <p className="text-sm text-muted-foreground">
+              isabella.nguyen@email.com
+            </p>
+          </div>
+          <div className="ml-auto font-medium">+$299.00</div>
+        </div>
+        <div className="flex items-center">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/avatars/04.png" alt="Avatar" />
+            <AvatarFallback>WK</AvatarFallback>
+          </Avatar>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">ویلیام کیم</p>
+            <p className="text-sm text-muted-foreground">will@email.com</p>
+          </div>
+          <div className="ml-auto font-medium">+$99.00</div>
+        </div>
+        <div className="flex items-center">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/avatars/05.png" alt="Avatar" />
+            <AvatarFallback>SD</AvatarFallback>
+          </Avatar>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">سوفیا دیویس</p>
+            <p className="text-sm text-muted-foreground">sofia.davis@email.com</p>
+          </div>
+          <div className="ml-auto font-medium">+$39.00</div>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+  <Bars/>
+  <div className="flex flex-row gap-4 justify-end">
+    <Waves/>
+    <Radial/>
+  </div>
+  <Noodle/>
+  </div>
+   
+  </main>
+</div>
+
   )
 }
