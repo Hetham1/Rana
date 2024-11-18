395e7a3305a79290522308dc3e41248d947fd142
Hethampc <jopjopjop372@gmail.com>
2024-11-18 19:21:21 +0330

    no more ts errors

--- admin_dashboard/admin/src/pages/Home.tsx
+++ admin_dashboard/admin/src/pages/Home.tsx
@@ -10,7 +10,6 @@ import {
 import {
   Avatar,
   AvatarFallback,
-  AvatarImage,
 } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
